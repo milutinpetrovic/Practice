7307c97c04b3a0bafdb601fa60cb6c74ce0f05cb
milutinpetrovic <milutin.p97@gmail.com>
2023-11-20 15:10:46 +0100

    id name change

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
     var fullName = document.getElementById('fullName');
     var age = document.getElementById('age');
     var email = document.getElementById('email');
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
+    var password = document.getElementById('password');
+    var rePassword = document.getElementById('rePassword');
 
 
 
