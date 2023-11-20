a2e1a0accb433f8b91ac27e5ba80069f9f87d1d0
milutinpetrovic <milutin.p97@gmail.com>
2023-11-20 14:58:44 +0100

    added script

---
+++ script.js
@@ -0,0 +1,12 @@
+//<script type="text/javascript">
+    var form = document.getElementById('form');
+    var userName = document.getElementById('userName');
+    var fullName = document.getElementById('fullName');
+    var age = document.getElementById('age');
+    var email = document.getElementById('email');
+    var pass1 = document.getElementById('pass1');
+    var pass2 = document.getElementById('pass2');
+
+
+
+ //   </script>
