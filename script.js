93fc730ca63b28b28a0495b4986475a26ed439af
milutinpetrovic <milutin.p97@gmail.com>
2023-11-21 14:40:37 +0100

    validation

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-//<script type="text/javascript">
-    var form = document.getElementById('form');
-    var userName = document.getElementById('userName');
-    var fullName = document.getElementById('fullName');
-    var age = document.getElementById('age');
-    var email = document.getElementById('email');
-    var password = document.getElementById('password');
-    var rePassword = document.getElementById('rePassword');
+function validateForm() {
+    var email = document.getElementById('email').value.trim();
+    var username = document.getElementById('username').value.trim();
+    var fullName = document.getElementById('fullName').value;
+    var age = document.getElementById('age').value;
+    var password = document.getElementById('password').value;
+    var repeatPassword = document.getElementById('repeatPassword').value;
 
+    // Simple validation
+    if (email === '' || username === '' || fullName === '' || age === '' || password === '' || repeatPassword === '') {
+        alert('All fields must be filled out');
+        return;
+    }
 
+    // Email format validation
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert('Invalid email format');
+        return;
+    }
 
- //   </script>
+    // Check for empty spaces in the username
+    if (username.indexOf(' ') !== -1) {
+        alert('Username cannot contain spaces');
+        return;
+    }
+
+    // Check username length
+    if (username.length > 16) {
+        alert('Username must be max 16 characters');
+        return;
+    }
+
+    // Check full name length
+    if (fullName.length > 256) {
+        alert('Full Name must be max 256 characters');
+        return;
+    }
+
+    // Check if age is at least 18
+    if (parseInt(age) < 18) {
+        alert('You must be at least 18 years old to register');
+        return;
+    }
+
+    // Password length and match validation
+    if (password.length < 8 || password.length > 256 || password !== repeatPassword) {
+        alert('Password must be between 8 and 256 characters, and the passwords must match');
+        return;
+    }
+
+    // Redirect to the success page
+    window.location.href = 'success.html';
+}
+
